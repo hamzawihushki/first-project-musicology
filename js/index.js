be5c4b725961d6ed694a192fc53a07ce82f8e5e0
hamzawihushki <149612147+hamzawihushki@users.noreply.github.com>
2024-02-14 22:31:24 +0300

    hamza 10:3pm 14 feb

--- js/index.js
+++ js/index.js
@@ -79,3 +79,41 @@ function openWhatsApp() {
   var url = "https://wa.me/" + phoneNumber;
   window.location.href = url;
 }
+$("#about-slider").slick({
+  autoplay: true,  // Enable autoplay
+  autoplaySpeed: 30000, // Autoplay speed in milliseconds (3 seconds)
+  arrows: false,  // Hide navigation arrows (optional)
+  dots: false,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  responsive: [
+    {
+
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        infinite: true,
+      },
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1,
+      },
+    },
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ],
+});
